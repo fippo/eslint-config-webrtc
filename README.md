fc45d26a12cd102b8200599a3fc5e1b7848c192a
Philipp Hancke <fippo@appear.in>
2016-04-30 21:27:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+##eslint shareable config for WebRTC and ORTC
+This modules defines a number of objects used by WebRTC and ORTC as global
+variables in a [shareable config](http://eslint.org/docs/developer-guide/shareable-configs)
+
+##usage
+```
+{
+    "extends": "webrtc"
+}
+```
+
+##License
+MIT
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    globals: {
+        MediaStream: true,
+        webkitMediaStream: true,
+        MediaStreamTrack: true,
+        mozRTCPeerConnection: true,
+        mozRTCSessionDescription: true,
+        mozRTCIceCandidate: true,
+        webkitRTCPeerConnection: true,
+        RTCPeerConnection: true,
+        RTCSessionDescription: true,
+        RTCRtpSender: true,
+        RTCRtpReceiver: true,
+        RTCIceTransport: true,
+        RTCDtlsTransport: true,
+        RTCIceGatherer: true,
+        RTCSctpTransport: true,
+        RTCCertificate: true
+    }
+}
